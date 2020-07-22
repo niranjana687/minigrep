c8d34fa362c94a083cb68a2377e9171e1ca72f05
niranjana687 <niranjana687@gmail.com>
2020-07-22 11:26:00 +0530

    refactoring complete

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,36 @@
 #![allow(unused_variables)]
 use std::env;
-use std::fs;
+
+use std::process;
+
+use minigrep::Config;
 
 fn main() {
     let args :Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    println!("Searching for: {}", config.query);
+    println!("In file: {}", config.filename);
 
-    // Saving the Argument Values in Variables
-    let query = &args[1];
-    let filename = &args[2];
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
 
-    println!("Searching for: {}", query);
-    println!("In file: {}", filename);
-
-    let contents = fs::read_to_string(filename)
-                    .expect("Something went wrong with reading the file");
-    
-    println!("With text: \n{}", contents);
-    
+        process::exit(1);
+    }
 }
+
+
+
+
+
+
+
+// fn parse_config(args: &[String]) -> Config {
+//     let config = Config {
+//         query: args[1].clone(),
+//         filename: args[2].clone(),
+//     };
+//     config
+// }
